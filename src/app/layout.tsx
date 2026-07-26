29c509dec965dec4f84705196866466cb98b10ab
Travis <travis@example.com>
2026-07-26 16:54:54 -0700

    chore: add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from '@clerk/nextjs';
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -35,6 +36,7 @@ export default function RootLayout({
       <body>
         <ClerkProvider>{children}</ClerkProvider>
         <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
