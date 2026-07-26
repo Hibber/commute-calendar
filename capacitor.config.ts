65420c5f1e31dc4c3d5f84d3f224dba0595612a5
Travis <travis@example.com>
2026-07-26 15:41:32 -0700

    chore: configure capacitor to use live server url

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.travis.commute',
   appName: 'commute-calendar',
-  webDir: 'out'
+  webDir: 'out',
+  server: {
+    url: 'https://schedule.triddle.dev',
+    cleartext: true
+  }
 };
 
 export default config;
